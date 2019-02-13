e05f8e4828c9990ee247ac8c2d0276d7b7290a9d
cristij <joracristi@gmail.com>
2019-02-13 23:53:14 +0200

    #22 Fix custom scroll behaviour blocking vue router scroll behavior

--- src/main.js
+++ src/main.js
@@ -16,7 +16,14 @@ Vue.use(LightBootstrap)
 // configure router
 const router = new VueRouter({
   routes, // short for routes: routes
-  linkActiveClass: 'nav-item active'
+  linkActiveClass: 'nav-item active',
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return {selector: to.hash}
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 /* eslint-disable no-new */
