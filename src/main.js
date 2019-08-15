f748f3f062ae9594012a41f26393494079ea5733
cristij <joracristi@gmail.com>
2019-08-15 19:16:13 +0300

    Add copyright comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*!
+
+ =========================================================
+ * Vue Light Bootstrap Dashboard - v2.0.0 (Bootstrap 4)
+ =========================================================
+
+ * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
