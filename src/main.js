d500c27adb33038881ed576fd7b889b212047871
Maisam Afshar <maisam.afshar@gmail.com>
2022-12-12 16:05:55 +0430

    update version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /*!
 
  =========================================================
- * Vue Light Bootstrap Dashboard - v2.0.0 (Bootstrap 4)
+ * Vue Light Bootstrap Dashboard - v2.1.0 (Bootstrap 4)
  =========================================================
 
  * Product Page: http://www.creative-tim.com/product/light-bootstrap-dashboard
- * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Copyright 2022 Creative Tim (http://www.creative-tim.com)
  * Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard/blob/master/LICENSE.md)
 
  =========================================================
@@ -13,37 +13,37 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
  */
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
 
 // LightBootstrap plugin
-import LightBootstrap from './light-bootstrap-main'
+import LightBootstrap from "./light-bootstrap-main";
 
 // router setup
-import routes from './routes/routes'
+import routes from "./routes/routes";
 
-import './registerServiceWorker'
+import "./registerServiceWorker";
 // plugin setup
-Vue.use(VueRouter)
-Vue.use(LightBootstrap)
+Vue.use(VueRouter);
+Vue.use(LightBootstrap);
 
 // configure router
 const router = new VueRouter({
   routes, // short for routes: routes
-  linkActiveClass: 'nav-item active',
+  linkActiveClass: "nav-item active",
   scrollBehavior: (to) => {
     if (to.hash) {
-      return {selector: to.hash}
+      return { selector: to.hash };
     } else {
-      return { x: 0, y: 0 }
+      return { x: 0, y: 0 };
     }
-  }
-})
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router
-})
+  el: "#app",
+  render: (h) => h(App),
+  router,
+});
